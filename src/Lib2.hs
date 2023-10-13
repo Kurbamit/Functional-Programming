34f9db5b584f3a6f76a365013ed5d8f3cf4a4aaf
Dominykas Černovas <cernovas50@gmail.com>
2023-10-13 13:06:41 +0300

    Added new function runSql that will run every SQL statement. Also implemented SHOW TABLES feature

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -3,25 +3,45 @@
 module Lib2
   ( parseStatement,
     executeStatement,
-    ParsedStatement
+    runSql,
+    ParsedStatement (..),
+    SQLCommand (..)
   )
 where
 
+import Data.Char
 import DataFrame (DataFrame)
-import InMemoryTables (TableName)
+import InMemoryTables (TableName, database)
 
 type ErrorMessage = String
 type Database = [(TableName, DataFrame)]
 
 -- Keep the type, modify constructors
-data ParsedStatement = ParsedStatement
+data ParsedStatement = SQLStatement SQLCommand
+  deriving (Show, Eq)
+
+data SQLCommand
+  = ShowTables
+  -- Define additional SQL commands like SUM, MIN, MAX here
+  deriving (Show, Eq)
 
 -- Parses user input into an entity representing a parsed
 -- statement
 parseStatement :: String -> Either ErrorMessage ParsedStatement
-parseStatement _ = Left "Not implemented: parseStatement"
+parseStatement input = case map toUpper input of
+  "SHOW TABLES" -> Right (SQLStatement ShowTables)
+  -- Add parsing logic for other SQL commands here
+  _ -> Left "Not implemented: parseStatement"
 
 -- Executes a parsed statemet. Produces a DataFrame. Uses
 -- InMemoryTables.databases a source of data.
-executeStatement :: ParsedStatement -> Either ErrorMessage DataFrame
-executeStatement _ = Left "Not implemented: executeStatement"
+executeStatement :: ParsedStatement -> Either ErrorMessage [TableName]
+executeStatement (SQLStatement command) = case command of
+  ShowTables -> Right $ map fst database
+  -- Implement execution for other SQL commands here
+  _ -> Left "Not implemented: executeStatement"
+
+runSql :: String -> Either ErrorMessage [TableName]
+runSql input = do
+  parsed <- parseStatement input
+  executeStatement parsed
