912743486feae0758d49b81222c55781b924226d
Rytis Karalius <90097195+rytiskar@users.noreply.github.com>
2023-10-19 17:39:00 +0000

    Implplemented basic SELECT functionality

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Lib2
   ( parseStatement,
@@ -14,6 +15,12 @@ import Data.Maybe (listToMaybe)
 import Data.List (find, isPrefixOf)
 import DataFrame (Column (..), ColumnType (..), Value (..), Row, DataFrame (..))
 import InMemoryTables (TableName, database)
+import Lib1 (parseSelectAllStatement)
+import GHC.Conc (par)
+import System.IO (putStrLn)
+import Debug.Trace ( trace )
+import Data.List (elemIndex)
+
 
 type ErrorMessage = String
 type Database = [(TableName, DataFrame)]
@@ -25,6 +32,7 @@ data ParsedStatement = SQLStatement SQLCommand
 data SQLCommand
   = ShowTables
   | ShowTableColumns TableName -- Add a new constructor for showing table columns
+  | Select TableName [String] 
   -- Define additional SQL commands like SUM, MIN, MAX here
   deriving (Show, Eq)
 
@@ -36,8 +44,31 @@ parseStatement input
   | otherwise = case map toLower <$> words input of
     ["show", "tables"] -> Right (SQLStatement ShowTables)
     ["show", "table", tableName] -> Right (SQLStatement (ShowTableColumns tableName))
+    ["select", columns, "from", tableName] -> Right (SQLStatement (Select tableName (words columns)))
     _ -> Left "Not implemented: parseStatement"
 
+selectFromTable :: TableName -> [String] -> Database -> Either ErrorMessage DataFrame
+selectFromTable tableName columns database = do
+  case findTable tableName database of
+    Just (DataFrame tableColumns tableRows) -> do
+      let selectedColumns = filterColumns tableColumns columns
+      let selectedRows = map (\row -> filterRow row (getColumnsWithIndexes selectedColumns tableColumns)) tableRows
+      return (DataFrame selectedColumns selectedRows)
+    Nothing -> Left "Table not found"
+
+filterColumns :: [Column] -> [String] -> [Column]
+filterColumns allColumns selectedColumns
+  | "*" `elem` selectedColumns = allColumns
+  | otherwise = filter (\(Column name _) -> name `elem` selectedColumns) allColumns
+
+getColumnsWithIndexes :: [Column] -> [Column] -> [(Int, Column)]
+getColumnsWithIndexes selectedColumns allColumns =
+  let getIndex column = case elemIndex column allColumns of
+        Just index -> index
+  in [(index, column) | column <- selectedColumns, let index = getIndex column]
+
+filterRow :: Row -> [(Int, Column)] -> Row
+filterRow row selectedColumns = [row !! index | (index, _) <- selectedColumns]
 
 -- Executes a parsed statemet. Produces a DataFrame. Uses
 -- InMemoryTables.databases a source of data.
@@ -47,6 +78,7 @@ executeStatement (SQLStatement command) = case command of
   ShowTableColumns tableName -> case findTable tableName database of
     Just (DataFrame columns _) -> Right $ DataFrame [Column "Columns" StringType] $ map (\(Column colName _) -> [StringValue colName]) columns
     Nothing -> Left "Table not found"
+  Select tableName columns -> selectFromTable tableName columns database
   -- Implement execution for other SQL commands here
   _ -> Left "Not implemented: executeStatement"
 
@@ -57,8 +89,8 @@ runSql input = do
   case executeStatement parsed of
     Right (DataFrame [Column _ StringType] rows) ->
       Right $ map (\(StringValue tableName : _) -> tableName) rows
+    Right _ -> Left "Invalid result format"
     Left err -> Left err
-    _ -> Left "Invalid result format"
 
 
 -- Helper function to perform case-insensitive lookup
