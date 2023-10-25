38266c99c562d95bc3980593525aef3d387b7884
Rytis Karalius <90097195+rytiskar@users.noreply.github.com>
2023-10-25 18:47:08 +0000

    Implemented functionality to enable usage of column in aggregate functions.

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -11,18 +11,15 @@ module Lib2
 where
 
 import Data.Char
-import Data.String (IsString, fromString)
 import Data.Maybe (listToMaybe)
 import Data.List (find, isPrefixOf)
 import DataFrame (Column (..), ColumnType (..), Value (..), Row, DataFrame (..))
 import InMemoryTables (TableName, database)
 import Lib1 (parseSelectAllStatement)
-import GHC.Conc (par)
-import System.IO (putStrLn)
-import Debug.Trace ( trace )
+import Debug.Trace ( trace, traceShow )
 import Data.List (elemIndex)
 import Data.List (isInfixOf)
-import Text.ParserCombinators.ReadP (string)
+import Data.List (sum)
 
 
 type ErrorMessage = String
@@ -35,9 +32,18 @@ data ParsedStatement = SQLStatement SQLCommand
 data SQLCommand
   = ShowTables
   | ShowTableColumns TableName -- Add a new constructor for showing table columns
-  | Select TableName [String]
+  | Select TableName [ColumnWithAggregate]
   -- Define additional SQL commands like SUM, MIN, MAX here
   deriving (Show, Eq)
+
+data ColumnWithAggregate = ColumnWithAggregate String (Maybe Aggregate)
+  deriving (Show, Eq)
+
+data Aggregate
+  = Max
+  | Sum
+  deriving (Show, Eq)
+
 
 -- Parses user input into an entity representing a parsed
 -- statement
@@ -49,14 +55,26 @@ parseStatement input =
         ["show", "table", tableName] -> Right (SQLStatement (ShowTableColumns (extractSubstring input tableName)))
         ("select" : columnNames) -> do
             let tableName = extractTableName columnNames
-            let columns = take (length columnNames - 2) columnNames
-            Right (SQLStatement (Select (extractSubstring input tableName) (map (extractSubstring input) columns)))
+            let columns = formColumnWithAggregateList (take (length columnNames - 2) columnNames)
+            -- Commented part below extracts original column names from the input which helps to do CASE-SENSITIVE
+            -- comparisons later. It is commented now because column names were represented as list of strings but that
+            -- is not a case anymore. Column names will be CASE-INSENSITIVE untill we fix this
+            Right (SQLStatement (Select (extractSubstring input tableName) {-map (extractSubstring input) -} columns))
         _ -> Left "Not implemented: parseStatement"
 
 extractTableName :: [String] -> String
 extractTableName ("from" : tableName : _) = tableName
-extractTableName (column : columns) = 
+extractTableName (column : columns) =
   let table = extractTableName columns in table
+
+formColumnWithAggregateList :: [String] -> [ColumnWithAggregate]
+formColumnWithAggregateList input = map extractNameFromAggregate input
+
+extractNameFromAggregate :: String -> ColumnWithAggregate
+extractNameFromAggregate input
+    | "max(" `isPrefixOf` input = ColumnWithAggregate (drop 4 (take (length input - 1) input)) (Just Max)
+    | "sum(" `isPrefixOf` input = ColumnWithAggregate (drop 4 (take (length input - 1) input)) (Just Sum)
+    | otherwise = ColumnWithAggregate input Nothing
 
 extractSubstring :: String -> String -> String
 extractSubstring input stringToMach =
@@ -76,29 +94,102 @@ findSubstringPosition string substring = findPosition 0 string
       | substring `isPrefixOf` string = (index, index + length substring - 1)
       | otherwise = findPosition (index + 1) xs
 
-selectFromTable :: TableName -> [String] -> Database -> Either ErrorMessage DataFrame
+findMax :: [Value] -> Value
+findMax values@(value:_) =
+  case value of
+    IntegerValue _ -> maxInt values
+    StringValue _ -> maxString values
+    BoolValue _ -> maxBool values
+    _ -> NullValue
+    where
+        maxInt :: [Value] -> Value
+        maxInt values = do
+            let valueList = [value | IntegerValue value <- values]
+            if null valueList then
+                NullValue
+            else
+                IntegerValue $ maximum valueList
+        maxString :: [Value] -> Value
+        maxString values = do
+            let valueList = [value | StringValue value <- values]
+            if null valueList then
+                NullValue
+            else
+                StringValue $ maximum valueList
+        maxBool :: [Value] -> Value
+        maxBool values = do
+            let valueList = [value | StringValue value <- values]
+            if null valueList then
+                NullValue
+            else
+                StringValue $ maximum valueList
+
+findSum :: [Value] -> Value
+findSum values@(value:_) =
+  case value of
+    IntegerValue _ -> sumInt values
+    _ -> NullValue
+    where
+        sumInt :: [Value] -> Value
+        sumInt values = do
+            let valueList = [value | IntegerValue value <- values]
+            if null valueList then
+                NullValue
+            else
+                IntegerValue $ sum valueList
+
+getColumnName :: ColumnWithAggregate -> String
+getColumnName (ColumnWithAggregate name _) = name
+
+getAggregate :: ColumnWithAggregate -> Maybe Aggregate
+getAggregate (ColumnWithAggregate _ aggregate) = aggregate
+
+findColumnByName :: DataFrame -> String -> Column
+findColumnByName (DataFrame columns _) columnName =
+    case filter (\(Column name _) -> name == columnName) columns of
+        (column : _) -> column
+
+findRows :: DataFrame -> String -> [Row]
+findRows (DataFrame tableColumns tableRows) name = map (\row -> filterRow row (getColumnsWithIndexes [findColumnByName (DataFrame tableColumns tableRows)  name] tableColumns)) tableRows
+
+applyAggregateToColumn :: DataFrame -> ColumnWithAggregate -> DataFrame
+applyAggregateToColumn dataframe (ColumnWithAggregate _ Nothing) = dataframe
+applyAggregateToColumn (DataFrame tableColumns tableRows) (ColumnWithAggregate name aggregate) =
+  case getAggregate (ColumnWithAggregate name aggregate) of
+    Just Max -> DataFrame tableColumns [valueToRow (findMax (rowToValue (findRows (DataFrame tableColumns tableRows) name)))]
+    Just Sum -> DataFrame tableColumns [valueToRow (findSum (rowToValue (findRows (DataFrame tableColumns tableRows) name)))]
+
+valueToRow :: Value -> Row
+valueToRow val = [val]
+
+rowToValue :: [Row] -> [Value]
+rowToValue rows = concatMap (\row -> row) rows
+
+applyAggregates :: DataFrame -> [ColumnWithAggregate] -> DataFrame
+applyAggregates = foldr (\column dataFrame -> applyAggregateToColumn dataFrame column)
+
+selectFromTable :: TableName -> [ColumnWithAggregate] -> Database -> Either ErrorMessage DataFrame
 selectFromTable tableName columns database =
   case findTable tableName database of
     Just (DataFrame tableColumns tableRows) ->
-      if "*" `elem` columns
+      if "*" `elem` map getColumnName columns
       then
-        let requestedColumns = tableColumns  -- Include all columns
-            selectedRows = map (\row -> filterRow row (getColumnsWithIndexes requestedColumns tableColumns)) tableRows
-        in Right (DataFrame requestedColumns selectedRows)
+        let selectedRows = map (\row -> filterRow row (getColumnsWithIndexes tableColumns tableColumns)) tableRows
+        in Right (DataFrame tableColumns selectedRows)
       else
         let columnNames = [name | Column name _ <- tableColumns]  -- Extract column names
-            nonExistentColumns = filter (`notElem` columnNames) columns
+            nonExistentColumns = filter (`notElem` columnNames)  (map getColumnName columns)
         in if not (null nonExistentColumns)
            then
              Left ("Column(s) not found: " ++ unwords nonExistentColumns)
            else
-             let requestedColumns = filter (\(Column name _) -> name `elem` columns) tableColumns
+             let requestedColumns = filter (\(Column name _) -> name `elem` map getColumnName columns) tableColumns
                  selectedRows = map (\row -> filterRow row (getColumnsWithIndexes requestedColumns tableColumns)) tableRows
-             in Right (DataFrame requestedColumns selectedRows)
+             in Right (applyAggregates (DataFrame requestedColumns selectedRows) columns)
     Nothing -> Left "Table not found"
 
 filterColumns :: [Column] -> [String] -> [Column]
-filterColumns allColumns selectedColumns  
+filterColumns allColumns selectedColumns
   | "*" `elem` selectedColumns = allColumns
   | otherwise = filter (\(Column name _) -> name `elem` selectedColumns) allColumns
 
@@ -122,7 +213,6 @@ executeStatement (SQLStatement command) = case command of
   Select tableName columns -> selectFromTable tableName columns database
   -- Implement execution for other SQL commands here
   _ -> Left "Not implemented: executeStatement"
-
 
 runSql :: String -> Either ErrorMessage [TableName]
 runSql input = do
